ba81624a697c06bbe88d08c31a7c7bf7618b6a52
dinesh <dheenu02@gmail.com>
2023-09-02 09:44:39 +0530

    practice

---
+++ src/main/java/KeybordAction.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class KeybordAction {
+    public static void main(String[] args) {
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://www.google.com/inputtools/try/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        driver.findElement(By.xpath("//textarea[@id='demobox']"))
+                .sendKeys("Hello world");
+        driver.findElement(By.xpath("//textarea[@id='demobox']"))
+                .sendKeys(Keys.ENTER);
+        driver.findElement(By.xpath("//textarea[@id='demobox']"))
+                .sendKeys("Hello");
+        driver.findElement(By.xpath("//textarea[@id='demobox']"))
+                .sendKeys(Keys.ENTER);
+        driver.findElement(By.xpath("//textarea[@id='demobox']"))
+                .sendKeys("world");
+        try
+        {
+            Thread.sleep(5000);
+        }catch (Exception e){}
+        driver.findElement(By.xpath("//textarea[@id='demobox']"))
+                .sendKeys(Keys.BACK_SPACE);
+    }
+}
